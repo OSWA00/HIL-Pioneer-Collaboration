403ffb1c2e4899c402adcf31213727040cd196ac
LABR1 <111610981+LABR1@users.noreply.github.com>
2022-09-13 18:24:36 -0500

    FEAT GANANCIAS

--- esp32_firmware/include/controller.h
+++ esp32_firmware/include/controller.h
@@ -1,11 +1,11 @@
 // controller.h
 
-#define ROBOT_LENGTH 1
+#define ROBOT_LENGTH 0.19
 
 /* lineal controller variables */
 namespace controller_lineal
 {
-    float kp = 1;
+    float kp = 1.8;
     float error = 0.0;
     float u_p = 0.0;
     float velocity_right = 0.0;
@@ -15,7 +15,7 @@ namespace controller_lineal
 /* Y controller variables */
 namespace controller_theta
 {
-    float kp = 1;
+    float kp = 12;
     float u_p = 0.0;
     float error = 0.0;
 }
